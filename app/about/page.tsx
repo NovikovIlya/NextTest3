0ef2e6d57b85061f4a91a60fc24c2a5b3f6de73a
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-11-01 14:06:00 +0300

    2

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
+ const fetchData = async ()=>{
+  try {
+    const response = await fetch('http://localhost:3000/api/data');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+}
+}
+
 const page = () => {
   return (
     <div>About me</div>
